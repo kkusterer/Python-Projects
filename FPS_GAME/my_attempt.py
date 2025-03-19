ce0aebfef0f70367b1e3f10c9e8ea8591baa5693
kkusterer <kkusterer@students.vlhs.com>
2025-03-19 21:32:13 +0000

    .

--- FPS_GAME/my_attempt.py
+++ FPS_GAME/my_attempt.py
@@ -9,40 +9,63 @@ print(f'Number of enimies (max is 5): {number_of_enimies}')
 print(f'Bullets: {Start_amo}')
 
 while True:
-    if number_of_enimies == '1':
-            start = input("Are you ready to start: y/n")
+    start = input('Do you want to start    y/n')
+    if start =='y':
+        if number_of_enimies == '1':
+            Shot1_in_1 = input("you encounter an enemy what do you do.   Shoot(1)  Exit(2)")
+            if Shot1_in_1 =='1':
+                        Bullets_arter_1_in_1 = int(Start_amo) - 2
+                        print(f'you hit him in 2 shots. you have {Bullets_arter_1_in_1} bullets')
 
-            if start == 'y':
-                print('starting.....')
-                Shot1 = input("you encounter an enemy what do you do.   Shoot(1)  Exit(2)")
-                if Shot1 =='1':
-                    Bullets_arter_1 = int(Start_amo) - 2
-                    print(f'you hit him in 2 shots. you have {Bullets_arter_1}')
+                        print("You have killed all of the enimies")
+                        print('Good Job Soldier')
+                        break 
 
-                    print("You have killed all of the enimies")
-                    print('Good Jod Soldier')
-                    break
+        if number_of_enimies == '2':
+                       
+            Shot1_in_2 = input("you encounter an enemy what do you do.   Shoot(1)  Exit(2)")
+            if Shot1_in_2 =='1':
+                        Bullets_arter_1_in_2 = int(Start_amo) - 2
+                        print(f'you hit him in 2 shots. you have {Bullets_arter_1_in_2} bullets')
 
-                if Shot1 =='2':
-                    print("As you run away like a babby he shoots you")
-                    print('You are DEAD')
-                    break 
-                  
-    if number_of_enimies == '2':
-         start = input("Are you ready to start: y/n")
+                        Move_forward = input('do you want to move forward   y/n')
+                        if Move_forward == 'y':
+                                Shot2_in_2 = input("you encounter an other enemy what do you do.   Shoot(1)  Exit(2)")
+                                if Shot2_in_2 =='1':
+                                    Bullets_arter_2_in_2 = int(Bullets_arter_1_in_2) - 3
+                                    print(f'you hit him in 3 shots. you have {Bullets_arter_2_in_2}')
 
-        if start == 'y':
-                print('starting.....')
-                Shot1 = input("you encounter an enemy what do you do.   Shoot(1)  Exit(2)")
-                if Shot1 =='1':
-                    Bullets_arter_1 = int(Start_amo) - 2
-                    print(f'you hit him in 2 shots. you have {Bullets_arter_1}')
+                                    print("You have killed all of the enimies")
+                                    print('Good Job Soldier')
+                                    break
+            
+        if number_of_enimies == '3':
+                       
+            Shot1_in_3 = input("you encounter an enemy what do you do.   Shoot(1)  Exit(2)")
+            if Shot1_in_3 =='1':
+                        Bullets_arter_1_in_3 = int(Start_amo) - 2
+                        print(f'you hit him in 2 shots. you have {Bullets_arter_1_in_3} bullets')
 
-                    print("You have killed all of the enimies")
-                    print('Good Jod Soldier')
-                    break
-                
-                if Shot1 =='2':
-                    print("As you run away like a babby he shoots you")
-                    print('You are DEAD')
-                    break   
+                        Move_forward = input('do you want to move forward   y/n')
+                        if Move_forward == 'y':
+                                Shot2_in_3 = input("you encounter an other enemy what do you do.   Shoot(1)  Exit(2)")
+                                if Shot2_in_3 =='1':
+                                    Bullets_arter_2_in_3 = int(Bullets_arter_1_in_3) - 3
+                                    print(f'you hit him in 3 shots. you have {Bullets_arter_2_in_3}')
+                                    
+                                    Move_forward = input('do you want to move forward   y/n')
+                                    if Move_forward == 'y':
+                                            Shot3_in_3 = input("you encounter an other enemy what do you do.   Shoot(1)  Exit(2)")
+                                            if Shot3_in_3 =='1':
+                                                Bullets_arter_3_in_3 = int(Bullets_arter_2_in_3) - 3
+                                                print(f'you hit him in 3 shots. you have {Bullets_arter_3_in_3}')
+                                                print("You have killed all of the enimies")
+                                                print('Good Job Soldier')
+                                                break
+
+    if Shot1_in_1 or Shot1_in_2 or Shot2_in_2 or Shot1_in_3 or Shot2_in_3 or Shot3_in_3 =='2':
+        print("you are a babby")
+        break
+    if Move_forward == 'n':
+        print("you are a babby")
+        break
