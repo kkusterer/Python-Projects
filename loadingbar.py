b4e282a99739b0b8036e3455c7ac1b50aff1c5a2
kkusterer <kkusterer@students.vlhs.com>
2025-04-22 18:57:09 +0000

    now works for Windows (using cls) and macOS/Linux (using clear).

--- loadingbar.py
+++ loadingbar.py
@@ -3,12 +3,12 @@ import time, os
 def pac(text, delay=0.5):
     print(text)
     time.sleep(delay)
-    os.system('clear')
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def pac_end(text, delay=1.0):
     print(text)
     time.sleep(delay)
-    os.system('clear') 
+    os.system('cls' if os.name == 'nt' else 'clear') 
 
 y_o_n_main = input("Do you wnat to download y or n")
 if y_o_n_main == 'y':
